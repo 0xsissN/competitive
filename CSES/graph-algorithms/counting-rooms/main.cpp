4829bf457e891421e5f7496aa148523f3ce3b261
0xsissN <nico22delcastillo07@gmail.com>
2025-02-12 15:38:11 -0400

    del top sort

--- CSES/graph-algorithms/counting-rooms/main.cpp
+++ CSES/graph-algorithms/counting-rooms/main.cpp
@@ -10,6 +10,7 @@
 #define N endl
 #define ull unsigned long long int
 #define ll long long int
+#define DBG(x) cout<<#x<<" = "<<(x)<<endl;
 
 using namespace std;
 
@@ -23,44 +24,56 @@ const int MOD = 998244353;
 const int tam = (int)2e5 + 5;
 
 ll cdiv(ll a, ll b) {
-	return a / b + ((a ^ b) > 0 && a % b);
+ return a / b + ((a ^ b) > 0 && a % b);
 }  // divide a by b rounded up
 ll fdiv(ll a, ll b) {
-	return a / b - ((a ^ b) < 0 && a % b);
+ return a / b - ((a ^ b) < 0 && a % b);
 }  // divide a by b rounded down
 
-vi topSort(vector<vi>& g, vi inGrade){
-	int n = sz(inGrade);
-	vi topSorted;
-	queue<int> q;
-	FOR(i, 0, n){
-		if(inGrade[i] == 0)
-			q.push(i);
-	}
-	
-	while(!q.empty()){
-		int node = q.front();
-		q.pop();
-		topSorted.pb(node);
-		for(int y : g[node]){
-			if(--inGrade[y] == 0)
-				q.push(y);
-		}
-	}
-	
-	if(sz(topSorted) < n)
-		topSorted.clear();
-	
-	return topSorted;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+int n, m;
+
+bool validate(int x, int y){
+    return (x >= 0 && x < n && y >= 0 && y < m);
+}
+
+void dfs(int x, int y, vector<vector<char>>& gr){
+    gr[x][y] = '#';
+
+    FOR(i, 0, 4){
+        int a = x + dx[i], b = y + dy[i];
+        if(validate(a, b) && gr[a][b] == '.'){
+            dfs(a, b, gr);
+        }
+    }
 }
 
 void solve() {
+    cin >> n >> m;
+    vector<vector<char>> gr(n, vector<char>(m));
+    FOR(i, 0, n){
+        FOR(j, 0, m){
+            cin >> gr[i][j];
+        }
+    }
 
+    int cont = 0;
+    FOR(i, 0, n){
+        FOR(j, 0, m){
+            if(gr[i][j] == '.'){
+                dfs(i, j, gr);
+                cont++;
+            }
+        }
+    }
+
+    cout << cont << N;
 }
 
 int main() { 
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
-    int T; cin >> T;
+    int T = 1;
     FOR(i, 0, T) {
         solve();
     }
